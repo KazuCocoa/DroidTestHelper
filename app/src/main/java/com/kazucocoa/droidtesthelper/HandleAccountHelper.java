44e6778df29d72446711bf390edec5a02838f276
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-02-26 23:06:23 +0900

    add permission for android m

--- app/src/main/java/com/kazucocoa/droidtesthelper/HandleAccountHelper.java
+++ app/src/main/java/com/kazucocoa/droidtesthelper/HandleAccountHelper.java
@@ -7,6 +7,8 @@ import android.accounts.AccountManagerFuture;
 import android.accounts.AuthenticatorException;
 import android.accounts.OperationCanceledException;
 import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.Toast;
@@ -30,25 +32,39 @@ public class HandleAccountHelper {
     public void removeAccount(@NonNull String accountType) {
         Account[] accountArray = accountManager.getAccountsByType(accountType);
 
-        for(final Account account : accountArray) {
-            accountManager.removeAccount(account,
-                    new AccountManagerCallback<Boolean>() {
-                        @Override
-                        public void run(AccountManagerFuture future) {
-                            try {
-                                future.getResult(20L, TimeUnit.SECONDS);
-                            } catch (OperationCanceledException oce) {
-                                Log.d(TAG, String.valueOf(oce));
-                            } catch (IOException ioe) {
-                                Log.d(TAG, String.valueOf(ioe));
-                            } catch (AuthenticatorException ae) {
-                                Log.d(TAG, String.valueOf(ae));
+        for (final Account account : accountArray) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                try {
+                    accountManager.removeAccount(account,
+                            null,
+                            new AccountManagerCallback<Bundle>() {
+                                @Override
+                                public void run(AccountManagerFuture future) {
+                                    try {
+                                        future.getResult(20L, TimeUnit.SECONDS);
+                                    } catch (OperationCanceledException | AuthenticatorException | IOException oce) {
+                                        Log.d(TAG, String.valueOf(oce));
+                                    }
+                                }
+                            }, null);
+                } catch (SecurityException se) {
+                    showToast("Can't remove account because of security exception.");
+                    return;
+                }
+            } else {
+                accountManager.removeAccount(account,
+                        new AccountManagerCallback<Boolean>() {
+                            @Override
+                            public void run(AccountManagerFuture future) {
+                                try {
+                                    future.getResult(20L, TimeUnit.SECONDS);
+                                } catch (OperationCanceledException | IOException | AuthenticatorException oce) {
+                                    Log.d(TAG, String.valueOf(oce));
+                                }
                             }
-                        }
-                    }, null);
+                        }, null);
+            }
             showToast("remove accounts associated with: " + accountType);
-            // TODO: We should use the following code when build with over android.os.Build.VERSION_CODES.LOLLIPOP_MR1
-            // accountManager.removeAccount(account, null, null, null).getResult(20L, TimeUnit.SECONDS);
         }
     }
 
--- app/src/main/java/com/kazucocoa/droidtesthelper/MainActivity.java
+++ app/src/main/java/com/kazucocoa/droidtesthelper/MainActivity.java
@@ -1,9 +1,18 @@
 package com.kazucocoa.droidtesthelper;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
 
     static String intentExtraAccountType = "accountType";
@@ -24,10 +33,37 @@ public class MainActivity extends AppCompatActivity {
             removeAccount();
         }
 
+        MainActivityPermissionsDispatcher.showGetAccountAndRemoveWithCheck(this);
+
+        showGetAccountAndRemove();
         finish();
     }
 
-    private void removeAccount() {
+    @NeedsPermission(Manifest.permission.GET_ACCOUNTS)
+    public void showGetAccountAndRemove() {
+        removeAccount();
+    }
+
+    @OnShowRationale(Manifest.permission.GET_ACCOUNTS)
+    void showRationaleForGetAccount(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.permission_get_account)
+                .setPositiveButton(R.string.button_allow, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton(R.string.button_deny, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.proceed();
+                    }
+                })
+                .show();
+    }
+
+    public void removeAccount() {
         HandleAccountHelper handleAccountHelper = new HandleAccountHelper(this);
         handleAccountHelper.removeAccount(accountType);
     }
